ff3116625af2a8846ea075a75a3148754a42ae7d
kishu279 <souravpoddar6677@gmail.com>
2024-12-03 16:57:13 +0530

    setting up project

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hii there this side Sourav");
+});
+
+app.listen(3000, () => {
+  console.log("Server Listening at port 3000...");
+});
